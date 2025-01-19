6b06b20d3d5230d55d64fd3023d25f90b198d9fb
omeldar <eldar.work.edu@gmail.com>
2025-01-19 19:00:04 +0100

    .ppm file-generation working

---
+++ app/ImageGenerator.hs
@@ -0,0 +1,35 @@
+module ImageGenerator (
+    -- types
+    Pixel, Row, Image,
+
+    -- functions
+    createPPM, ppmToStr, createAndWriteFile
+) where
+
+type Pixel = (Int, Int, Int)   -- (R,G,B)
+type Row = [Pixel]
+type Image = [Row]
+
+createPPM :: Int -> Int -> Image
+createPPM width height = 
+    [[(r, g, b) | x <- [0..width-1],
+                  let r = scale x width,
+                  let g = scale y height,
+                  let b = 0]
+                | y <- [0..height-1]]
+    where 
+        scale val maxVal = round (((255.999 :: Double) * fromIntegral val) / fromIntegral (maxVal - 1))
+
+ppmToStr :: Image -> String
+ppmToStr image = 
+        let height = if null image then 0 else length image
+            width = if null image then 0 else length (head image)
+            header = "P3\n" ++ show width ++ " " ++ show height ++ "\n255\n"
+            pixelData = unlines $ map (unwords . map showPixel) image
+        in header ++ pixelData
+    where 
+        showPixel :: Pixel -> String
+        showPixel (r, g, b) = unwords $ map show [r, g, b]
+
+createAndWriteFile :: String -> String -> IO ()
+createAndWriteFile path content = writeFile path content
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,25 @@
 module Main where
 
+import System.Environment (getArgs)
+import Data.Time (getCurrentTime, formatTime, defaultTimeLocale)
+import ImageGenerator as IG
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    args <- getArgs -- Retrieve command-line args
+    let (width, height) = parseArgs args
+    putStrLn $ "Width: " ++ show width ++ ", Height: " ++ show height
+
+    currentTime <- getCurrentTime
+    let timestamp = formatTime defaultTimeLocale "%Y%m%d-%H%M%S" currentTime
+    let filename = "out/" ++ timestamp ++ ".ppm"
+
+    IG.createAndWriteFile filename $ IG.ppmToStr $ IG.createPPM width height
+    putStrLn $ "File written to: " ++ filename
+
+-- Parse the command-line arguments
+parseArgs :: [String] -> (Int, Int)
+parseArgs [] = (250, 250)  -- assign default
+parseArgs [a] = let val = read a in (val, val)  -- if only one value: img becomes square with size of that value
+parseArgs [a, b] = (read a, read b)  -- image size fixed on both sides by args
+parseArgs _ = error "Too many arguments. Provide at most two arguments."
