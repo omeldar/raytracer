f7eda62f512feb1d437d5ae92b243044e89d3b93
omeldar <eldar.work.edu@gmail.com>
2025-01-26 02:50:22 +0100

    Add colored progress bars

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,18 @@ module Main where
 import System.Environment (getArgs)
 import Data.Time (getCurrentTime, formatTime, defaultTimeLocale, diffUTCTime)
 
+import ProgressBar
+import Control.Concurrent (threadDelay)
+
 import ImageGenerator as IG
+    ( createPPM, ppmToStr, createAndWriteFile )
 
 main :: IO ()
 main = do
     args <- getArgs -- Retrieve command-line args
     let (width, height) = parseArgs args
     putStrLn $ "Width: " ++ show width ++ ", Height: " ++ show height
+    putStrLn ""
 
     currentTime <- getCurrentTime
     let timestamp = formatTime defaultTimeLocale "%Y%m%d-%H%M%S" currentTime
@@ -18,10 +23,53 @@ main = do
     startFileCreation <- getCurrentTime
     IG.createAndWriteFile filename $ IG.ppmToStr $ IG.createPPM width height
     endFileCreation <- getCurrentTime
+    
+    pb <- newProgressBar 32418
+    simTask pb
 
     let timeToCreate = diffUTCTime endFileCreation startFileCreation
+    putStrLn ""
     putStrLn $ "Time taken for create and write file: " ++ show timeToCreate
     putStrLn $ "File written to: " ++ filename
+
+simTask :: ProgressBar -> IO()
+simTask pb = do
+    updateMessage pb "Starting tasks..."
+
+    threadDelay 2000000
+    updateMessage pb "Running..."
+    updateProgress pb 313
+
+    threadDelay 2000000
+    updateProgress pb 518
+
+    threadDelay 2000000
+    updateProgress pb 738
+
+    threadDelay 1300000
+    updateProgress pb 4594
+    
+    threadDelay 500000
+    updateProgress pb 11532
+
+    threadDelay 500000
+    updateProgress pb 17477
+
+    threadDelay 500000
+    updateProgress pb 24867
+
+    threadDelay 1300000
+    updateProgress pb 30786
+
+    threadDelay 1700000
+    updateMessage pb "Finalizing"
+    updateProgress pb 32417
+
+    threadDelay 3000000
+    updateMessage pb "Task complete!"
+    updateProgress pb 32418
+
+    putStrLn ""
 
 -- Parse the command-line arguments
 parseArgs :: [String] -> (Int, Int)
---
+++ app/ProgressBar.hs
@@ -0,0 +1,98 @@
+module ProgressBar (
+    ProgressBar,
+    newProgressBar,
+    updateProgress,
+    updateMessage,
+    renderProgressBar
+) where
+
+import Control.Monad (when)
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
+import System.IO (hFlush, stdout)
+import System.Console.ANSI (setSGR, SGR(..), ConsoleLayer(..), Color(..), ColorIntensity(..), setTitle)
+import GHC.Clock (getMonotonicTime)
+import Data.Time.Clock(diffUTCTime, getCurrentTime)
+
+progressBarWidth :: Int
+progressBarWidth = 50
+
+data ProgressBar = ProgressBar 
+    { progress      :: IORef Float
+    , message       :: IORef String
+    , startTime     :: IORef Double
+    , currentSteps  :: IORef Int
+    , totalSteps    :: IORef Int
+    }
+
+newProgressBar :: Int -> IO ProgressBar
+newProgressBar total = do
+    progressRef     <- newIORef 0.0
+    messageRef      <- newIORef ""
+    startTimeRef    <- newIORef =<< getMonotonicTime
+    currentStepsRef <- newIORef 0
+    totalStepsRef   <- newIORef total
+    return $ ProgressBar progressRef messageRef startTimeRef currentStepsRef totalStepsRef
+
+updateProgress :: ProgressBar -> Int -> IO ()
+updateProgress pb steps = do
+    writeIORef (currentSteps pb) steps
+    total <- readIORef (totalSteps pb)
+    writeIORef (progress pb) (fromIntegral steps / fromIntegral total)
+    renderProgressBar pb
+
+updateMessage :: ProgressBar -> String -> IO ()
+updateMessage pb newMessage = do
+    writeIORef (message pb) newMessage
+    renderProgressBar pb
+
+renderProgressBar :: ProgressBar -> IO ()
+renderProgressBar pb = do
+    currentProgress <- readIORef (progress pb)
+    currentMessage  <- readIORef (message pb)
+    currentSteps    <- readIORef (currentSteps pb)
+    totalSteps      <- readIORef (totalSteps pb)
+    startTime       <- readIORef (startTime pb)
+    currentTime     <- getMonotonicTime
+
+    let elapsedTime     = currentTime - startTime
+        stepsPerSecond  = fromIntegral currentSteps / elapsedTime
+        filledWidth     = round (currentProgress * fromIntegral progressBarWidth)
+        filled          = replicate filledWidth '▓'
+        empty           = replicate (progressBarWidth - filledWidth) '░'
+    
+    setTitle "Raytracer"
+    setSGR [SetColor Foreground Vivid Blue]
+    putStr $ "\r[" ++ filled ++ empty ++ "] "
+
+    let progressPercent = (floor (currentProgress * 10000) :: Int) `div` 100
+        stepsPerSec     = (round (stepsPerSecond * 100) :: Int) `div` 100
+
+    setSGR [SetColor Foreground Vivid Green]
+    putStr $ show (progressPercent :: Int) ++ "% "
+
+    setSGR [SetColor Foreground Vivid Yellow]
+    putStr $ show (currentSteps :: Int) ++ "/" ++ show (totalSteps :: Int) ++ " "
+
+    setSGR [SetColor Foreground Vivid Red]
+    putStr $ show (stepsPerSec :: Int) ++ "/s "
+
+    setSGR [SetColor Foreground Vivid Cyan]
+    putStr $ formatTime elapsedTime ++ " "
+
+    setSGR [SetColor Foreground Vivid Black]
+    putStr currentMessage
+    setSGR [Reset]
+
+    hFlush stdout
+
+formatTime :: Double -> String
+formatTime seconds =
+    let hours = floor (seconds / 3600)
+        minutes = floor ((seconds - fromIntegral hours * 3600) / 60)
+        secs = floor (seconds - fromIntegral hours * 3600 - fromIntegral minutes * 60)
+    in show hours ++ ":" ++ show minutes ++ ":" ++ show secs
+
+withProgressBar :: Int -> (ProgressBar -> IO a) -> IO a
+withProgressBar total action = do
+    pb <- newProgressBar total
+    action pb
