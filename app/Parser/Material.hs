0d3f09e4c095325ed4ad93da8cc9b5dca8c8a315
Eldar Omerovic <eldar.work.edu@gmail.com>
2025-04-04 15:09:55 +0200

    Create material parsing module

--- app/Parser/Material.hs
+++ app/Parser/Material.hs
@@ -1,4 +1,22 @@
 module Parser.Material where
 
-a :: Int
-a = 5
+import qualified Data.Map as M
+import Rendering.Material
+import Core.Vec3 (Vec3 (..))
+
+type MaterialMap = M.Map String (V.Vec3, MaterialType)
+
+parseMaterial :: String -> MaterialMap
+parseMaterial content = parseLines (lines content) M.empty Nothing
+    where
+        parseLines [] acc _ = acc
+        parseLines (l:ls) acc currentName =
+            case worlds l of
+                ("newmtl" : name : _) -> parseLines ls acc (Just name)
+                ("Kd" : r : g : b : _) -> 
+                    case currentName of
+                        Just name -> 
+                            let color = Vec3 (read r) (read g) (read b)
+                            in parseLines ls (M.insert name (color, Lambertian) acc) name
+                        Nothing -> parseLines ls acc currentName
+                _ -> parseLines ls acc currentName
